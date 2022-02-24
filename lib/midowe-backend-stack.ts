5a2ce3d5de673c5ad9e03b11470afc4d45fb9374
Americo Chaquisse <achaquisse1@gmail.com>
2022-02-24 10:48:28 +0200

    Initial commit

---
+++ lib/midowe-backend-stack.ts
@@ -0,0 +1,16 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class MidoweBackendStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'MidoweBackendQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
